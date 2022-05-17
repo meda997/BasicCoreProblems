cfe19f8ab7432df5c935007d0f16e0838447d3e0
meda997 <medatejaswi7@gmail.com>
2022-05-18 03:06:16 +0530

    [tejaswi];added code for checking even or odd

---
+++ BasicCorePrograms/BasicCorePrograms/EvenOdd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class EvenOdd
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Enter a Number : ");
+            i =Convert.ToInt32(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Entered Number is an Even Number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Entered Number is an Odd Number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+    
