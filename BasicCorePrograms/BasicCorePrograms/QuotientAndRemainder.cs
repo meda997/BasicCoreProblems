86b58cf176765e4eb0392c44483cf205a011c683
meda997 <medatejaswi7@gmail.com>
2022-05-18 02:42:52 +0530

    [tejaswi];added code for quotient and remainder

---
+++ BasicCorePrograms/BasicCorePrograms/QuotientAndRemainder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class QuotientAndRemainder
+    {
+        public void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int quotient;
+            int remainder;
+
+            Console.WriteLine("Enter the first number : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            quotient = num1 / num2;
+            remainder = num1 % num2;
+
+            Console.WriteLine("Quotient is : " + quotient);
+            Console.WriteLine("Remainder is : " + remainder);
+        }
+    }
+}
