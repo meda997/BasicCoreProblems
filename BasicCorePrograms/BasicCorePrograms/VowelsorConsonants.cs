a616cce66c0df596ed3f7188f373ac2d524ac183
meda997 <medatejaswi7@gmail.com>
2022-05-18 03:13:30 +0530

    [tejaswi];added code for checking vowel or consonant

---
+++ BasicCorePrograms/BasicCorePrograms/VowelsorConsonants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class VowelsorConsonants
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character:");
+            char input = Convert.ToChar(Console.ReadLine());
+            if (input == 'a' || input == 'A' || input == 'e' || input == 'E' || input == 'i' || input == 'I' || input == 'o'
+                    || input == 'O' || input == 'u' || input == 'U')
+            {
+                Console.WriteLine("Its a vowel");
+            }
+            else if ((input >= 'a' && input <= 'z') || (input >= 'A' && input <= 'Z'))
+            {
+
+                Console.WriteLine("Its a consonant");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input please enter an alphabet");
+            }
+        }
+    }
+}
+
