ec636eac02f1df21db545c90e3d012af3617f912
meda997 <medatejaswi7@gmail.com>
2022-05-18 02:58:41 +0530

    [tejaswi];added code for swaping two numbers

---
+++ BasicCorePrograms/BasicCorePrograms/Swaptwonumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Swaptwonumbers
+    {
+    
+        public static void Main(string[] args)
+        {
+            int number1, number2, temp;
+            Console.Write("Input the First Number : ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the Second Number : ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.Write("After Swapping : ");
+            Console.Write("First Number : " + number1);
+            Console.Write("Second Number : " + number2);
+            Console.Read();
+        }
+    }
+}
+}
