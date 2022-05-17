c5d2b0813af05127197aa9cf44525ff18770c99f
meda997 <medatejaswi7@gmail.com>
2022-05-18 01:46:12 +0530

    [tejaswi];added code to check leap year

---
+++ BasicCorePrograms/BasicCorePrograms/Leapyear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Leapyear
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Year : ");
+            int Year = Convert.ToInt32(Console.ReadLine());
+            {
+                if ((Year % 4 == 0) && (Year % 400 == 0))
+                    Console.WriteLine("{0} is a Leap Year.", Year);
+
+
+                else
+
+                    Console.WriteLine("{0} is not a Leap Year.", Year);
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
